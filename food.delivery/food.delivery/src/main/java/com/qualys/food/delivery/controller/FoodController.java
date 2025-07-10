8368163905040df4c761ba924f09c733e35cb05f
Siddhesh <siddheshmca@gmail.com>
2025-07-10 19:58:49 +0530

    Basic Files

---
+++ food.delivery/food.delivery/src/main/java/com/qualys/food/delivery/controller/FoodController.java
@@ -0,0 +1,15 @@
+package com.qualys.food.delivery.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FoodController {
+
+	@RequestMapping("/hello")
+	public String welcomeUser() {
+		String message = "Welcome Siddhesh";
+		return message;
+	}
+
+}
